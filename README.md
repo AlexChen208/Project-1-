41444dd675220d52a88a0ec7e67ea2ccf82190e1
AlexChen208 <alex.chencs207@gmail.com>
2022-06-09 13:00:30 -0700

    finializing HTML/CSS/JavaScript

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 <<<<<<< HEAD
-# Concentration Game
-Code a game of Concentration 
+# Memory Game
+<p>Collect all color pairs before the time runs out! Try to memorize where each color is at when you click on two different colors until you match a pair!
 <img width="1440" alt="Screen Shot 2022-06-09 at 10 30 48 AM" src="https://user-images.githubusercontent.com/104876014/172912837-c0868d29-317e-46d2-803e-532c5bcae220.png">
 =======
 
-# Concentration Game
 
 ![Screenshot of game](Concentration screenshot.png)
 
-<p>Technologies used: HTML/CSS/Javascript</p>
+<p>Technologies used: HTML/CSS/JavaScript</p>
 
-[link]()
->>>>>>> bd80487 (Creating win/loss logic of game)
+[MemoryGame](https://AlexChen208.github.io/Project-1-/)
--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 
-/*----- constants -----*/
-const colors = [
-    'red',
-    'green',
-    'blue',
-    'teal',
-    'purple',
-    'cyan',
-    'yellow',
-    'maroon'
-]
 /*----- app's state (variables) -----*/
 // State - something that is always changing, something that the browser has to keep changing
 let firstClick = ''
@@ -20,17 +9,76 @@ let combos = 0
 let timerFun = null
 /*----- cached element references -----*/
 // Using more than once
-const board = document.getElementById('board')
+const boardEl = document.getElementById('board')
 const timerEl = document.getElementById('timer')
 const readyButton = document.getElementById('readyButton')
 const message = document.getElementById('message')
 /*----- event listeners -----*/
-board.addEventListener('click', flippedCard)
+boardEl.addEventListener('click', flippedCard)
 readyButton.addEventListener('click', getTimer)
 
 
 
-/*----- functions -----*/
+/*----- functions -----*/  
+function flippedCard(event) {
+    if (event.target.className.includes('matched') || firstClick === event.target) {
+        return
+    }
+    if (!firstClick) {
+        firstClick = event.target
+        firstClick.classList.remove('color-hidden')
+    } else {
+        secondClick = event.target
+        secondClick.classList.remove('color-hidden')
+        boardEl.removeEventListener('click', flippedCard)
+        if (firstClick.getAttribute('data-color') !== secondClick.getAttribute('data-color')) {
+            setTimeout(function () {
+                firstClick.classList.add('color-hidden')
+                secondClick.classList.add('color-hidden')
+                firstClick = ''
+                secondClick = ''
+                boardEl.addEventListener('click', flippedCard)
+            }, 1000)
+        } else {
+            firstClick.classList.add('matched')
+            secondClick.classList.add('matched')
+            firstClick = ''
+            secondClick = ''
+            boardEl.addEventListener('click', flippedCard)
+            combos++
+            if (combos === 8) {
+                endGame(true)
+            }
+        }
+    }
+}
+
+function getTimer() {
+    timerFun = setInterval(function() {
+        const minute = Math.floor(time / 60)
+        let seconds = time % 60
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        timerEl.innerHTML = `${minute}:${seconds}`
+        time--
+        if (time === -1) {
+            endGame(false)
+        }
+    }, 1000)
+}
+
+function endGame(won) {
+    if (won) {
+        message.innerHTML = 'You won the game'
+    } else {
+        message.innerHTML = 'You lose'
+    }
+    clearInterval(timerFun)
+    boardEl.removeEventListener('click', flippedCard)
+}
+ 
+ 
+
+
 
 // Create a function that checks for pairs
     // Grab firstClick and store value while revealing the color
@@ -43,74 +91,14 @@ readyButton.addEventListener('click', getTimer)
             // else cards do match, have a class name matched to know that the cards do match
                 // set clicks back to empty string, don't store previous data
                 // enable click function
-                // apply win/loss logic at the bottom to keep track of score    
-function flippedCard(event) {
+                // apply win/loss logic at the bottom to keep track of score   
 
-    if (event.target.className.includes('matched') || firstClick === event.target) {
-        return
-    }
-
-    if (!firstClick) {
-        firstClick = event.target
-        firstClick.classList.remove('color-hidden')
-    } else {
-        secondClick = event.target
-        secondClick.classList.remove('color-hidden')
-        board.removeEventListener('click', flippedCard)
-        if (firstClick.getAttribute('data-color') !== secondClick.getAttribute('data-color')) {
-            setTimeout(function () {
-                firstClick.classList.add('color-hidden')
-                secondClick.classList.add('color-hidden')
-                firstClick = ''
-                secondClick = ''
-                board.addEventListener('click', flippedCard)
-            }, 1000)
-        } else {
-            firstClick.classList.add('matched')
-            secondClick.classList.add('matched')
-            firstClick = ''
-            secondClick = ''
-            board.addEventListener('click', flippedCard)
-            combos++
-            if (combos === 8) {
-                endGame(true)
-            }
-        }
-    }
-
-}
-
-  
 // Create a timer for the game
     // Calculate the time that I want to show on the timer and how many seconds to go down at a time
     // If time reaches zero, end the game
- function getTimer() {
-    timerFun = setInterval(function () {
-        const minute = Math.floor(time / 60)
-        let seconds = time % 60
-        seconds = seconds < 10 ? '0' + seconds : seconds
-        timerEl.innerHTML = `${minute}:${seconds}`
-        time--
-        if (time === -1) {
-            endGame(false)
-        }
-    }, 1000)
- }
-  
+
 //  Create win/loss logic
     // If won is true
         // Print winner
     // else 
         // print loser
- function endGame(won) {
-    if (won) {
-        message.innerHTML = 'You won the game'
-    } else {
-        message.innerHTML = 'You lose'
-    }
-    clearInterval(timerFun)
-    board.removeEventListener('click', flippedCard)
- }
- 
- 
-
